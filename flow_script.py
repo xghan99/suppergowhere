0f551cc853b558633a09d34091ed2b8685d8db33
xghan99 <xghan99@hotmail.com>
2023-06-20 00:36:16 +0800

    First commit

---
+++ flow_script.py
@@ -0,0 +1,68 @@
+from prefect import flow, task
+import requests
+import pandas as pd
+import numpy as np
+import geopy.distance
+import pytz
+from datetime import datetime, date
+from prefect_gcp import GcpCredentials
+
+
+
+#Helper Functions
+def get_num_taxis(x,y):
+  dists = y.map(lambda z: geopy.distance.geodesic(z,x).m)
+  num_taxis = (dists<500).sum()
+  return num_taxis
+
+#Extract
+@task(log_prints = True)
+def get_data_from_api(year,month,day,hour):
+    query_string = f"{year}-{month:02}-{day:02}T{hour:02}%3A00%3A00"
+    content = requests.get(f"https://api.data.gov.sg/v1/transport/taxi-availability?date_time={query_string}")
+    data = content.json()['features'][0]['geometry']['coordinates']
+    df = pd.DataFrame(data, columns = ['lng','lat'])
+    df = df[['lat','lng']]
+    df['combined'] = df.values.tolist()
+    return df
+
+#Transform
+@task(log_prints = True)
+def compute_taxis_per_station(mrt_data,df,hour,weekday, today_date):
+    new_df = mrt_data.copy()
+    new_df['num_taxis'] = new_df['combined'].map(lambda x: get_num_taxis(x, df['combined']))
+    new_df['hour'] = hour
+    new_df['day'] = weekday
+    new_df['date'] = today_date
+    new_df['date'] = pd.to_datetime(new_df['date'], infer_datetime_format = True)
+    result = new_df[['date','station_name','day','hour','num_taxis']]
+    return result
+
+#Load to BQ
+@task
+def load_to_bq(df):
+  gcp_credentials_block = GcpCredentials.load("supper-01")
+  df.to_gbq(destination_table = "taxi_mrt_stations.frequency", project_id = "suppergowhere",
+            credentials = gcp_credentials_block.get_credentials_from_service_account(),
+            if_exists = "append")
+    
+@flow
+def main_flow():
+    #Reading MRT Data
+    mrt_data = pd.read_csv("mrt_lrt_data.csv")
+    mrt_data = mrt_data[mrt_data['type'] == 'MRT']
+    mrt_data['combined'] = mrt_data[['lat','lng']].values.tolist()
+    #Getting Arguments
+    now = datetime.now(pytz.timezone("singapore"))
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    weekday = now.weekday()
+    today_date = date.today()
+    #Running Tasks
+    df = get_data_from_api(year,month,day,hour)
+    df_transformed = compute_taxis_per_station(mrt_data,df,hour, weekday, today_date)
+    load_to_bq(df_transformed)
+
+
---
+++ frontend/app.py
@@ -0,0 +1,58 @@
+import dash
+import dash_core_components as dcc
+from dash import html,Input, Output, State
+import plotly.graph_objects as go
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+app = dash.Dash(__name__)
+server = app.server
+"""
+credentials = service_account.Credentials.from_service_account_file(
+    '../suppergowhere-658e4c3c35a8.json') #Private service account credentials
+"""
+project_id = 'suppergowhere'
+client = bigquery.Client(project = project_id)
+#client = bigquery.Client(project = project_id, credentials = credentials)
+day_mapping = {'Monday':0,'Tuesday':1,'Wednesday':2,'Thursday':3,'Friday':4,'Saturday':5,'Sunday':6}
+
+app.layout = html.Div(children=[
+    html.H1(children='SupperGoWhere', className = 'title'),
+    html.Div(children='Where to have supper so that you can afford the Grab home?', className='subtitle'),
+    html.Br(),
+    html.Div(children='Enter the day and time you are planning to head home', className = 'instructions'),
+    html.Div(children = [dcc.Dropdown(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'], placeholder='Select the Day', id='day', clearable = False),
+    dcc.Dropdown(['00:00','01:00','02:00','03:00','04:00','05:00'], id = 'time',placeholder='Select the Time', clearable = False),
+    html.Button('Go', n_clicks = 0, id = 'submit')], id = 'user_input'),
+    html.Br(),
+    html.Div(id = 'output', children = '')
+    ])
+
+@app.callback(
+    Output('output', 'children'),
+    Input('submit','n_clicks'),
+    State('day','value'),
+    State('time','value')
+
+)
+
+def update_output(n_clicks, day_value,time_value):
+    if n_clicks>0:
+        day = day_mapping[day_value]
+        time = int(time_value[1])
+        query = f"SELECT station_name, AVG(num_taxis) as avg_taxi \
+            FROM taxi_mrt_stations.frequency \
+            WHERE day = {day} AND hour = {time} \
+            GROUP BY station_name, day, hour \
+            ORDER BY avg_taxi DESC \
+            LIMIT 5"
+        query_job = client.query(query)
+        result = query_job.to_dataframe()
+        child_components = [html.Div("The top 5 MRT stations to have supper nearby are: ")]
+        for i in result['station_name']:
+            child_components.append(html.Div(f"{i}"))
+
+        return html.Div(children = child_components)
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', port=8080, debug=True)
