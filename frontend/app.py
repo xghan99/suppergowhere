e5e2c9eed124bf2f5492c1dba701d6c45ec2bbca
Xiao Guang <50982061+xghan99@users.noreply.github.com>
2023-08-20 20:08:02 +0800

    Updated app.py with comments

--- frontend/app.py
+++ frontend/app.py
@@ -12,18 +12,18 @@ app = Dash(__name__, suppress_callback_exceptions=True, meta_tags=[{"name": "vie
 if not local_testing:
     server = app.server
     
-project_id = 'suppergowhere'
+project_id = 'suppergowhere'  #For Google Cloud Platform
 
 # Load the day mapping
 with open("./data/mapping.json", "r") as f:
     day_mapping = json.load(f)
 
-# Load the MRT data
+# Load the MRT Stations' coordinates data
 mrt = MRT("./data/mrt_lrt_data.csv")
 mrt_coord_dict = mrt.get_mrt_coord_dict(mrt.data)
 
 
-# init PlaceSearcher and BigQuery Wrapper classes
+# init PlaceSearcher (for getting supper spots) and BigQuery (for getting top 5 MRT stations/ cached supper spots) Wrapper classes
 place_searcher = PlaceSearcher(
     local_testing=local_testing, mrt_coord_dict=mrt_coord_dict, project_id = project_id)
 
@@ -62,7 +62,7 @@ app.layout = html.Div(children=[
     className='container'
 )
 
-
+#This runs upon user submitting the day and time
 @app.callback(
     Output('output', 'children'),
     Input('submit', 'n_clicks'),
@@ -72,9 +72,10 @@ app.layout = html.Div(children=[
 def update_output(n_clicks, day_value, time_value):
     global day_mapping
     if n_clicks > 0:
+        #Convert day to a numerical value ('Monday' = 0 etc.)
         day = day_mapping[day_value]
         time = int(time_value[1])
-        # TODO: refactor out the query
+        #Query for top 5 MRT stations with highest average number of taxis around
         query = f"SELECT station_name, AVG(num_taxis) as avg_taxi \
             FROM taxi_mrt_stations.frequency \
             WHERE day = {day} AND hour = {time} \
@@ -86,23 +87,24 @@ def update_output(n_clicks, day_value, time_value):
         child_components = [
             html.Div("The top 5 MRT stations to have supper nearby are: ")]
         tabs = []
-        count = 0
+        count = 0  
         initial_name = ''
+        #Create tabs for each of the top 5 results
         for i in result['station_name']:
             if count==0:
-                initial_name = i
+                initial_name = i #Set the default tab to be the MRT station with the highest average number of taxis around
             tabs.append(dcc.Tab(label = f'{i}', value = f'{i}'))
             count+=1
         child_components.append(dcc.Tabs(children=tabs, id="selected_location", value = initial_name))
         return html.Div(children=child_components)
 
-
+#This runs upon user clicking on an MRT stataion
 @app.callback(
     Output('selected_place', 'children'),
     Input('selected_location', 'value'),
     State('day', 'value'),
     State('time', 'value'),
-    prevent_initial_call=True
+    prevent_initial_call=True  #This ensures that the function does not run when user first enters the page
 )
 def suggest_all_places(loc, day, time):
     day = day_mapping[day]
